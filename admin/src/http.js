aad4bf7f40d3c9d4a5eeb96e9f691b580b0a63f4
leohuo <reason0127301@gmail.com>
2021-11-01 15:41:45 +0800

    2-19-2

--- admin/src/http.js
+++ admin/src/http.js
@@ -1,12 +1,16 @@
 import axios from "axios";
 import Vue from "vue";
+import router from "./router";
+
 const http = axios.create({
   baseURL: "http://localhost:3000/admin/api"
 });
 http.interceptors.request.use(
   function(config) {
     // Do something before request is sent
-    config.headers.Authorization = "Bearer " + localStorage.token;
+    if (localStorage.token) {
+      config.headers.Authorization = "Bearer " + localStorage.token;
+    }
     return config;
   },
   function(error) {
@@ -25,6 +29,10 @@ http.interceptors.response.use(
         type: "error",
         message: err.response.data.message
       });
+
+      if (err.response.status === 401) {
+        router.push("/login");
+      }
     }
     return Promise.reject(err);
   }
