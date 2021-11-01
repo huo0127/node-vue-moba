fe9868b236a8ee253c1ef776a3315d9a6f532621
leohuo <reason0127301@gmail.com>
2021-11-01 16:31:16 +0800

    2-21

--- admin/src/main.js
+++ admin/src/main.js
@@ -13,6 +13,21 @@ Vue.config.productionTip = false;
 import http from "./http";
 Vue.prototype.$http = http;
 
+Vue.mixin({
+  computed: {
+    uploadUrl() {
+      return this.$http.defaults.baseURL + "/upload";
+    }
+  },
+  methods: {
+    getAuthHeaders() {
+      return {
+        Authorization: `Bearer ${localStorage.token || ""}`
+      };
+    }
+  }
+});
+
 /* eslint-disable no-new */
 new Vue({
   el: "#app",
